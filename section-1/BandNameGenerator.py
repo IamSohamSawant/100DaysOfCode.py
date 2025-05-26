b521995e2bd7845e9b11f65e0cb84b8a2ebc8d0b
IamSohamSawant <153931177+IamSohamSawant@users.noreply.github.com>
2025-05-27 01:16:03 +0530

    Initial commit - Section 1 project

---
+++ section-1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+name1=(input("What's the name of the city you grew up in? "" \n"))
+name2=(input("what's your pet's name?""\n"))
+print("Your Band name could be: "+ name1  +"" +name2)
